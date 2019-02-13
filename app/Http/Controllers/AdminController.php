202ca6bda0c60e2677f4d4fcbcc850785c2d11b6
Francisco Javier Rios Bautista <franciscojavier.rios.bautista@iesvegademijas.es>
2019-02-13 10:55:12 +0100

    Modificacion de AdminController (falta modificacion)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,13 +9,18 @@ class AdminController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        if( !empty( auth() ) ){
+            return redirect('/login');
+        }else{
+            if( (auth()->user()->admin === 0) ){
+                return redirect('/home');
+            }
+        }
 
     }
 
     public function index(){
-        if(auth()->user()->admin){
-            return "hello sir";
-        }
-        return "noob";
+        //return "hello sir ".((1==1)?"true":"false");
+        return "hello sir ".((auth()->user()->admin == 0)?"true":"false");
     }
 }
